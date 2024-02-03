e860c310bdb9a66e8185b4a129db1caa9944c3a1
MarkosComK <markoscomks@gmail.com>
2024-02-03 11:20:11 +0000

    exercises from book

---
+++ Cap01/prog0106.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void	main()
+{
+	printf("1 -    Clientes\n");
+	printf("2 -    Fornecedores\n");
+	printf("3 -    Facturas\n\n");
+	printf("0 -    Sair\n");
+}
---
+++ Cap01/progEx15.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void	main(void)
+{
+	printf("Bem Vindos ao /Miundo\\ da programacao em \"C\"\n");
+}
