921060fef917da7bbc8274f4934421461ca316ee
MarkosComK <markoscomks@gmail.com>
2024-02-12 21:09:45 +0000

    implemented solution

--- Cap03/prog0306.c
+++ Cap03/prog0306.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 20:53:27 by marsoare          #+#    #+#             */
-/*   Updated: 2024/02/12 21:04:37 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/02/12 21:08:57 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,12 @@ int	main(void)
 	scanf("%f", &salary);
 	if (salary <= 0)
 		printf("Salary cannot be zero or negative.\n");
-	else if (salary > 1000)
-		printf("Your tax is %.2f (10%%)\n", salary / 100 * 10);
 	else
-		printf("Congratulations! You don`t pay any taxes.\n");
+	{
+		if (salary > 1000)
+			printf("Your tax is %.2f (10%%)\n", salary / 100 * 10);
+		else
+			printf("Your tax is %.2f (5%%)\n", salary / 100 * 5);
+	}
 	return (0);
 }
