c32cb38f22dac29ef8bed78723efc44cbcf60c86
MarkosComK <markoscomks@gmail.com>
2024-02-18 22:30:19 +0000

    leap year working

---
+++ Cap03/prog0323.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prog0323.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/18 22:19:35 by marsoare          #+#    #+#             */
+/*   Updated: 2024/02/18 22:30:06 by marsoare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	main(void)
+{
+	int	year;
+
+	printf("Type a year to check if its a leap year: ");
+	scanf("%d", &year);
+
+	if (year % 4 == 0)
+	{
+		if (!(year % 100 == 0))
+		{
+			printf("Leap year!");
+			break;
+		}
+		if (year % 400 ==0)
+			printf("Leap year!");
+		else
+			printf("Isnt a leap year");
+	}
+	return (0);
+}
